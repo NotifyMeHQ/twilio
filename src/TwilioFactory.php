1984c9e8c66dee4aa57be5c19e4f6d5e61c7c868
Graham Campbell <graham@alt-three.com>
2015-06-27 11:15:16 +0100

    Updated for the new contracts

--- src/TwilioFactory.php
+++ src/TwilioFactory.php
@@ -13,7 +13,7 @@ namespace NotifyMeHQ\Twilio;
 
 use GuzzleHttp\Client;
 use NotifyMeHQ\NotifyMe\Arr;
-use NotifyMeHQ\NotifyMe\FactoryInterface;
+use NotifyMeHQ\Contracts\FactoryInterface;
 
 class TwilioFactory implements FactoryInterface
 {
--- src/TwilioGateway.php
+++ src/TwilioGateway.php
@@ -13,7 +13,7 @@ namespace NotifyMeHQ\Twilio;
 
 use GuzzleHttp\Client;
 use NotifyMeHQ\NotifyMe\Arr;
-use NotifyMeHQ\NotifyMe\GatewayInterface;
+use NotifyMeHQ\Contracts\GatewayInterface;
 use NotifyMeHQ\NotifyMe\HttpGatewayTrait;
 use NotifyMeHQ\NotifyMe\Response;
 
@@ -70,7 +70,7 @@ class TwilioGateway implements GatewayInterface
      * @param string   $message
      * @param string[] $options
      *
-     * @return \NotifyMeHQ\NotifyMe\Response
+     * @return \NotifyMeHQ\Contracts\ResponseInterface
      */
     public function notify($to, $message, array $options = [])
     {
@@ -152,7 +152,7 @@ class TwilioGateway implements GatewayInterface
      * @param bool  $success
      * @param array $response
      *
-     * @return \NotifyMeHQ\NotifyMe\Response
+     * @return \NotifyMeHQ\Contracts\ResponseInterface
      */
     protected function mapResponse($success, $response)
     {
